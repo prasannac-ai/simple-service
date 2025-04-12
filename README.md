6cd7507a8ae31acb2c06f8669140b2d93406f009
prasannac-ai <179297542+prasannac-ai@users.noreply.github.com>
2025-04-12 18:30:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple-service
+
+uvicorn server:restapp --host 0.0.0.0 --port 8111
---
+++ server.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+restapp = FastAPI()
+@restapp.get("/")
+async def root():
+    return {"message": "Hello World RestAPI from FastAPI"}
+
+
+@restapp.get("/health")
+async def health():
+    return {"status": "healthy"}
