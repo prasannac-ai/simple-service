4c345acb981146bb658bf4f43799b38fcca77eb0
prasannac-ai <179297542+prasannac-ai@users.noreply.github.com>
2025-04-12 18:55:26 +0530

    Dockerize

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 pip install -r requirements.txt
 
 uvicorn server:restapp --host 0.0.0.0 --port 8111
+
+
+docker build -t simple-service .        
+
+docker images    
+
+docker run -p 8111:8111 simple-service 
+
+docker build -t simple-service:v2 .   
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 restapp = FastAPI()
 @restapp.get("/")
 async def root():
-    return {"message": "Hello World RestAPI from FastAPI"}
+    return {"message": "Hello World RestAPI from FastAPI V2"}
 
 
 @restapp.get("/health")
